97286ef5bdcfd0a320e010b584e7f212c1342de7
MMuzaf <mmuzaf@gmail.com>
2016-09-09 18:35:14 +0300

    Update groovy test for server thread

--- src/test/groovy/ServerThreadSpec.groovy
+++ src/test/groovy/ServerThreadSpec.groovy
@@ -1,5 +1,38 @@
+import easycs.ServerThread
+import easycs.data.ClientMetaData
+import easycs.network.IOSocketChannel
+import spock.lang.Specification
+
 /**
  * @author Mmuzafarov
  */
-class ServerThreadSpec {
+class ServerThreadSpec extends Specification {
+
+    class ServerThreadWithoutAuth extends ServerThread {
+        ServerThreadWithoutAuth(Socket socket) throws IOException {
+            super(socket)
+        }
+        @Override
+        public boolean clientAuthentication() {
+            return true;
+        }
+    }
+
+    ServerThread serverThread;
+
+    def "Execution server command successful"() {
+        setup:
+            def channel = Mock(IOSocketChannel)
+            def socket = Mock(Socket)
+            socket.getOutputStream() >> Mock(OutputStream)
+            socket.getInputStream() >> Mock(InputStream)
+
+            serverThread = new ServerThreadWithoutAuth(Mock(Socket))
+            serverThread.channel >> channel
+            serverThread.clientMetaData >> Mock(ClientMetaData)
+        when:
+            serverThread.run()
+        then:
+            1 == 1
+    }
 }
